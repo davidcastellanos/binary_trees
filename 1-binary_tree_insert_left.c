31833469c1fd01b502a0838e0c9d9c0010cfc64e
davidcastellanos <castellanos.johann@gmail.com>
2020-03-13 21:58:24 -0500

    [Update] Fixed condition if and new node ptr to left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - function that inserts a node as the left-child of another node
+ * @parent: Pointer to parent node
+ * @value: value
+ * Return: New node pointer or NULL on failure or NULL parent
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *nuevo_nodo = '\0';
+
+	if (!parent)
+		return ('\0');
+	nuevo_nodo = binary_tree_node(parent, value);
+	if (!nuevo_nodo)
+		return ('\0');
+	if (parent->left)
+	{
+		parent->left->parent = nuevo_nodo;
+		nuevo_nodo->left = parent->left;
+	}
+	parent->left = nuevo_nodo;
+	return (nuevo_nodo);
+}
